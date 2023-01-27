d0403dec5cfc770abd5604551c39b5051c624185
Noah Waldner <noah.waldner@digitecgalaxus.ch>
2023-01-27 23:09:09 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import { Template } from "@walletpass/pass-js";
+import fs from "fs";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+
+const createPass = async () => {
+  const template = await Template.load("./Familienkarte.pass", undefined, {
+    allowHttp: true,
+  });
+  template.setCertificate(process.env.APPLE_CERT, "examplepass");
+  template.setPrivateKey(process.env.APPLE_PK, "examplepass");
+  const pass = template.createPass();
+
+  pass.serialNumber = "schwarz1";
+  pass.description = "Familienkarte";
+  pass.primaryFields.add({
+    key: "name",
+    label: "Name",
+    value: "Noah Waldner",
+  });
+  pass.secondaryFields.add({
+        "key": "cardType",
+        "label": "Kartentyp",
+        "value": "Schwarz"
+  });
+  pass.locations = [
+    {
+      longitude: 47.53966,
+      latitude: 7.60681,
+    },
+  ];
+  pass.barcodes = [
+    {
+      message: "https://www.dasviertel.ch/viertelklub",
+      format: "PKBarcodeFormatQR",
+      messageEncoding: "iso-8859-1",
+    },
+  ];
+
+  const buf = await pass.asBuffer();
+  await fs.writeFile("./example.pkpass", buf, () => {});
+  console.log(buf);
+};
+
+createPass();
